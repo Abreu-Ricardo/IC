91fde8dd33487be188ab664f109a1aa377f008d8
Abreu-Ricardo <61216670+Abreu-Ricardo@users.noreply.github.com>
2022-01-11 14:32:50 -0400

    Explicando um pouco do paramentros

--- gromacs/script_instalacao_GROMACS.sh
+++ gromacs/script_instalacao_GROMACS.sh
@@ -5,19 +5,24 @@
 cd gromacs-2021.4;
 mkdir build;
 cd build;
-cmake .. -DGMX_BUILD_OWN_FFTW=ON -DREGRESSIONTEST_DOWNLOAD=ON -DGMX_GPU=OpenCL; #-DGMX_GPU=ON;
+cmake .. -DGMX_BUILD_OWN_FFTW=ON -DREGRESSIONTEST_DOWNLOAD=ON -DGMX_GPU=ON -DGMX_GPU=OpenCL GMX_USE_RDTSCP=ON; #-DGMX_GPU=ON;
 make;
 make check;
 sudo make install;
 source /usr/local/gromacs/bin/GMXRC;
 
+# Habilita a GPU
+# -DGMX_GPU=ON
 
+# Especifica o tipo de API para a GPU
+# -DGMX_GPU=OpenCL
+
+# Quando a CPU pode medir tempos melhores do que o especificado por padrao
+# GMX_USE_RDTSCP=ON
 
 ## coisas extras que precisa
 # Make e Cmake
 # fftw - Biblioteca de transformacoes de Fourier
 # libc-dev
-
-###### Se tiver GPU e nao for NVIDIA usar o que esta embaixo
 ## sudo apt update
 ## sudo apt install ocl-icd-opencl-dev
