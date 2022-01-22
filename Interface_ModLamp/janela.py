81d2f30f007de1c285000c68d519e1ac3abff733
Abreu-Ricardo <61216670+Abreu-Ricardo@users.noreply.github.com>
2022-01-22 02:02:22 -0400

    Implementar parte da IA ainda
    Arrumei as posicoes das Box e dos Grids para ficar melhor de ver.

--- Interface_ModLamp/janela.py
+++ Interface_ModLamp/janela.py
@@ -3,8 +3,10 @@ from hashlib import new
 from pickle import TRUE
 from struct import pack
 from tkinter import HORIZONTAL
+from tkinter.tix import Tree
 from typing import Sequence
 import gi
+from matplotlib.pyplot import grid
 
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk, GLib
@@ -26,24 +28,38 @@ import sample_class
 
 class Janela(Gtk.Window):
     def __init__(self):
-        super().__init__(title="Minha Janela")
+        super().__init__(title="ModLamp Grafico")
         self.set_size_request(800, 400)
 
+        # grid1
         self.grid = Gtk.Grid()
         self.grid.set_row_spacing(10)
+        self.grid.set_column_homogeneous(True)
          
         self.seq_text = Gtk.Label()
-        self.seq_text.set_label("Teste de label:   ")
+        self.seq_text.set_label("Sequência:")
 
 
         self.entrada = Gtk.Entry()
         self.entrada.set_placeholder_text("Insira sua sequência aqui...")
 
+        # Grid para dividir ao meio a janela e depois anexar os itens nessa box
+        self.box_base =Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6, homogeneous=True)
+        # self.box_base.set_row_spacing(10)
+        # self.box_base.set_column_homogeneous(True)
+
+        self.add(self.box_base)
 
 
+        # grid2
+        self.grid2 = Gtk.Grid()
+        self.grid2.set_row_spacing(10)
+        self.grid2.set_column_homogeneous(True)
 
-        # TESTE APAGAR OU MELHORAR
-        vbox1 = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+
+        # MELHORAR
+        vbox1 = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=13, homogeneous=True)
+        
 
         self.text_p_1 = Gtk.Label(label="Hidrofobicidade:           ", selectable=TRUE)
         self.text_p_2 = Gtk.Label(label="Valor da carga:            ", selectable=TRUE)
@@ -67,8 +83,12 @@ class Janela(Gtk.Window):
         vbox1.pack_start(self.text_p_6, TRUE, TRUE, 0)
 
 
+        self.grid2.attach(vbox1, 0, 0, 1, 1)
+        
+
+
         # Vbox da frente
-        vbox2 = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+        vbox2 = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=13, homogeneous=True)
         self.textVetor = []
 
         for i in range(0,6):
@@ -78,6 +98,26 @@ class Janela(Gtk.Window):
         for i in range(0,6):
             vbox2.pack_start(self.textVetor[i], TRUE, TRUE, 0)
 
+
+        self.grid2.attach(vbox2, 1, 0, 1, 1)
+
+
+
+        # Escalas
+        self.nome_escalas = ["AASI", "eisenberg", "gravy", "argos", "MSS", "MSW", "pepcats", "polarity", "PPCALI", "TM_tend", "refractivity",]
+
+        # ComboBox
+        self.comboBox = Gtk.ComboBoxText()
+        for i in self.nome_escalas:
+            self.comboBox.append_text(i)
+
+        self.comboBox.connect("changed", self.troca_escala, self)
+
+
+
+        # TESTE ************
+        # self.box_base.attach(vbox1, 0, 0, 1, 1)
+        # self.box_base.attach(vbox2, 1, 0, 1, 1)
 
 
 
@@ -97,12 +137,23 @@ class Janela(Gtk.Window):
         self.grid.attach(self.botao_salva,    1, 3, 1, 1)
 
 
-        self.grid.attach(vbox1, 1, 5, 1, 1)
-        self.grid.attach(vbox2, 2, 5, 1, 1)
+        # Vbox's
+        self.box_base.pack_start(self.grid, True, True, 0)
+        self.box_base.pack_start(self.grid2, True, True, 0)
+
+        # ComboBox
+        self.grid.attach(self.comboBox, 3,1,1,1)
+
+        teste_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=6)
+
+        #self.box_base.attach(self.grid ,  0, 0, 1, 1)
+        #self.box_base.attach(self.grid2,  0, 1, 1, 1)
+        #self.box_base.attach(self.grid2,  0, 1, 1, 1)
 
 
 
-        self.add(self.grid)
+        #self.add(self.grid)
+        #self.add(vbox1)
     
     #Passa o objeto janela como 1 parametro e o objeto botao no 2
     def calcular(aux1, aux2, self):
@@ -135,6 +186,14 @@ class Janela(Gtk.Window):
     
     def salvar(janela, botao):
         print("Implementar funcao para salvar num arquivo txt todas as propriedades fisico-quimicas")
+
+    # N da para trocar a escala, pois usa PeptideDescriptor e os calculos
+    # sao feitos com o GlobalDescriptor
+    def troca_escala(aux1, aux2, self):
+        escala_selecionada = self.comboBox.get_active_text()
+
+        print(f"Escala selecionada -> {self.comboBox.get_active_text()}")
+        #sample_class.descriptors.PeptideDescriptor(scalename=escala_selecionada)
 
 
 if __name__ == '__main__':
