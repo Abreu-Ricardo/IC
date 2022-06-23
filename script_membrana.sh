d46d02bbc2f8bcf77a3389cfb1eb4796a3a1e955
Abreu-Ricardo <61216670+Abreu-Ricardo@users.noreply.github.com>
2022-06-22 23:39:46 -0400

    os parâmetros -c estavam com minimization(errado)
    Tinha que ser equilibration

--- script_membrana.sh
+++ script_membrana.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 ## minimizacao
-# gmx grompp -f step6.0_minimization.mdp -o step6.0_minimization.tpr -c step5_input.gro -r step5_input.gro -p topol.top -n index.ndx;
-# gmx mdrun -v -deffnm step6.0_minimization;
+gmx grompp -f step6.0_minimization.mdp -o step6.0_minimization.tpr -c step5_input.gro -r step5_input.gro -p topol.top -n index.ndx;
+gmx mdrun -v -deffnm step6.0_minimization;
 
 echo "DEU CERTO A PARTE DA MINIMIZACAO";
 echo " ";
@@ -14,19 +14,19 @@ echo " ";
 gmx grompp -f step6.1_equilibration.mdp -o step6.1_equilibration.tpr -c step6.0_minimization.gro -r step5_input.gro -p topol.top -n index.ndx;
 gmx mdrun -v -deffnm step6.1_equilibration;
 
-gmx grompp -f step6.2_equilibration.mdp -o step6.2_equilibration.tpr -c step6.1_minimization.gro -r step5_input.gro -p topol.top -n index.ndx;
+gmx grompp -f step6.2_equilibration.mdp -o step6.2_equilibration.tpr -c step6.1_equilibration.gro -r step5_input.gro -p topol.top -n index.ndx;
 gmx mdrun -v -deffnm step6.2_equilibration;
 
-gmx grompp -f step6.3_equilibration.mdp -o step6.3_equilibration.tpr -c step6.2_minimization.gro -r step5_input.gro -p topol.top -n index.ndx;
+gmx grompp -f step6.3_equilibration.mdp -o step6.3_equilibration.tpr -c step6.2_equilibration.gro -r step5_input.gro -p topol.top -n index.ndx;
 gmx mdrun -v -deffnm step6.3_equilibration;
 
-gmx grompp -f step6.4_equilibration.mdp -o step6.4_equilibration.tpr -c step6.3_minimization.gro -r step5_input.gro -p topol.top -n index.ndx;
+gmx grompp -f step6.4_equilibration.mdp -o step6.4_equilibration.tpr -c step6.3_equilibration.gro -r step5_input.gro -p topol.top -n index.ndx;
 gmx mdrun -v -deffnm step6.4_equilibration;
 
-gmx grompp -f step6.5_equilibration.mdp -o step6.5_equilibration.tpr -c step6.4_minimization.gro -r step5_input.gro -p topol.top -n index.ndx;
-gmx mdrun -v -deffnm step6.1_equilibration;
+gmx grompp -f step6.5_equilibration.mdp -o step6.5_equilibration.tpr -c step6.4_equilibration.gro -r step5_input.gro -p topol.top -n index.ndx;
+gmx mdrun -v -deffnm step6.5_equilibration;
 
-gmx grompp -f step6.6_equilibration.mdp -o step6.6_equilibration.tpr -c step6.5_minimization.gro -r step5_input.gro -p topol.top -n index.ndx;
+gmx grompp -f step6.6_equilibration.mdp -o step6.6_equilibration.tpr -c step6.5_equilibration.gro -r step5_input.gro -p topol.top -n index.ndx;
 gmx mdrun -v -deffnm step6.6_equilibration;
 
 
